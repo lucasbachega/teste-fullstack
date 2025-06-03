e7382c4602a791319931f9ca201bc549f595f936
Lucas Bachega <lucas.cbachega@gmail.com>
2025-06-03 16:25:56 -0300

    feat(books): implement create and list endpoints for Book resource

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { Book } from './schemas/book.schema';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto): Promise<Book> {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Get()
+  findAll(): Promise<Book[]> {
+    return this.booksService.findAll();
+  }
+}
--- backend/src/books/books.service.ts
+++ backend/src/books/books.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateBookDto } from './dto/create-book.dto';
+import { Book, BookDocument } from './schemas/book.schema';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(@InjectModel(Book.name) private bookModel: Model<BookDocument>) {}
+
+  async create(createBookDto: CreateBookDto): Promise<Book> {
+    return this.bookModel.create(createBookDto);
+  }
+
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
+  }
+}
