300d3bbf831cd3ad498fd676d8eebadf1ccf1f4c
Lucas Bachega <lucas.cbachega@gmail.com>
2025-06-03 15:09:25 -0300

    chore: config Mongoose Module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 9100;
+  await app.listen(port);
+  console.log(`NestJS API is running on http://localhost:${port}`);
 }
+
 bootstrap();
