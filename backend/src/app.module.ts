4b0140790396f4bd88930813af593f50ddedee13
Lucas Bachega <lucas.cbachega@gmail.com>
2025-06-03 16:14:08 -0300

    feat(books): add Book module with schema, DTO and basic setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT ?? 9100;
   await app.listen(port);
   console.log(`NestJS API is running on http://localhost:${port}`);
